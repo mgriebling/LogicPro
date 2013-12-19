9d77e37907d66812d30792257c9159c10f786352
Michael Griebling <bob@c-inspirations.com>
2013-12-19 05:55:39 -0500

    Cleaned up comments in LPAnd.

--- LogicPro/LPAnd.m
+++ LogicPro/LPAnd.m
@@ -43,36 +43,6 @@ const CGFloat IHEIGHT_AND = 217.0;
     [path setLineWidth:[self strokeWidth]];
     [LPGraphicPrimitives drawAndAtPoint:self.bounds.origin withPath:path atScale:scale];
     
-//    for (LPPin *pin in self.pins) {
-//        // Figure out a rectangle that's centered on the point but lined up with device pixels.
-//        CGRect pinBounds;
-//        pinBounds.origin.x = self.bounds.origin.x - LPGatePinHalfWidth;
-//        pinBounds.origin.y = self.bounds.origin.y - LPGatePinHalfWidth;
-//        pinBounds.size.width = LPGatePinWidth;
-//        pinBounds.size.height = LPGatePinWidth;
-////        pinBounds = [view convertRect:pinBounds toView:view];
-//        //    PinBounds = [view centerScanRect:PinBounds];
-//        
-//        // Draw the shadow of the Pin.
-////        CGRect pinShadowBounds = CGRectOffset(pinBounds, 1.0f, 1.0f);
-////        UIBezierPath *pinPath = [UIBezierPath bezierPathWithRect:pinShadowBounds];
-////        [[UIColor darkGrayColor] set];
-////        [pinPath fill];
-////        [path appendPath:pinPath];
-//        
-//        UIBezierPath *pinPath = [UIBezierPath bezierPathWithRect:pinBounds];
-////        [[UIColor redColor] set];
-////        [pinPath fill];
-//        [path appendPath:pinPath];
-//        
-////        [[UIColor darkGrayColor] set];
-////        UIRectFill(pinShadowBounds);
-//        
-//        // Draw the Pin itself.
-////        [[UIColor redColor] set];
-////        UIRectFill(pinBounds);
-//    }
-    
     return path;
     
 }
